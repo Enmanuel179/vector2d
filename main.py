0a96a7bd5e6b70189d74e10b31547894de280009
Enmanuel179 <121184121+Enmanuel179@users.noreply.github.com>
2023-02-24 16:18:53 +0000

    HW for 2/14 to 2/23

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 class Vector2d:
     pass
+def __init__(self, x:3, y:4):
+    x = self.x
+    x = self.y
+
+def __repr__(x, y):
+    x = 3
+    y = 4
+    (3, 4) == eval(repr(Vector2d(3, 4)))
+    return True
+
+def __str__(self,x, y):
+    self.x = x + i
+    self.y = y + j
+
+def__abs__(self, x, y):
